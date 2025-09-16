daffa4e2d4feced55e557e4a82ff242263081d23
Jake Shankman <s9158257@dekalbschoolsga.org>
2025-09-15 22:14:17 -0400

    more stuff

--- markob.dev/src/App.tsx
+++ markob.dev/src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import "bitrain-matrixx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <main>
+        <matrixx-canvas
+          rain-display="charamask"
+          direction="down"
+          density="6"
+          cell-size="16"
+          speed="20"
+          tail-min="4"
+          tail-max="12"
+        ></matrixx-canvas>
+        <h1 class="landing_title">markob.dev</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          You are really not meant to see this its just a landing page for all
+          my stuffs :)
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
